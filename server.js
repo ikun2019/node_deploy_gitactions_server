71672809dc9f31b87642aef2851e96265de60f90
ikun <0215.miyatto@gmail.com>
2023-06-30 05:36:25 +0900

    edit server.js

--- server.js
+++ server.js
@@ -10,14 +10,22 @@ app.use(cors());
 app.use(express.json());
 
 // * nuxt関連
-// app.use(express.static(path.join(__dirname, 'public')));
-// // Nuxt.jsアプリケーションのルートハンドラの設定
-// const { Nuxt } = require('nuxt');
-// const config = require('../client/nuxt.config.js');
-// config.dev = false;
-// const nuxt = new Nuxt(config);
-// // Nuxt.jsのルートハンドラを使用する
-// app.use(nuxt.render);
+app.use(express.static(path.join(__dirname, 'public')));
+// Nuxt.jsアプリケーションのルートハンドラの設定
+const { Nuxt, Builder } = require('nuxt');
+const config = require('../client/nuxt.config.js');
+config.dev = false;
+async function start() {
+  const nuxt = new Nuxt(config);
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+  // Nuxt.jsのルートハンドラを使用する
+  app.use(nuxt.render);
+}
 
 // * routerをマウント
 app.use(postsRouter);
@@ -26,3 +34,5 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running. PORT = ${PORT}`);
 })
+
+start();
